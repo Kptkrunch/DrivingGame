0d7cadabf1dccba7a9f25fd371e29243148ff31e
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-06-22 18:57:04 -0500

    game largely completed

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 packageColor = new Color32(1, 1, 0, 1);
+    [SerializeField] Color32 originalColor = new Color32(1, 0, 0, 1);
+
+    SpriteRenderer spriteRenderer; 
+
     bool hasPackage = false;
     [SerializeField] float destroyDelay = 0.5f;
+
+    void private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("whammo!!!");
     }
     
-    private void OnTriggerEnter2D(Collider2D other) {
+    void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "package") {
             Debug.Log("Package picked up!");
+
             hasPackage = true;
+            spriteRenderer.color = packageColor;
             Destroy(other.gameObject, destroyDelay);
         }
         
         if(other.tag == "customer" && hasPackage) {
             Debug.Log("Delivery complete");
             hasPackage = false;
+            spriteRenderer.color = originalColor;
         }
     }
 }
