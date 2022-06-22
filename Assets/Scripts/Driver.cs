9a2f4a3cf6ad6e28c2e691af9ea6ae10c3785f48
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-06-22 13:51:51 -0500

    Vehicle turn and acceleration completed

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float turnSpeed = .1f;
+    [SerializeField] float speed = .01f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float turnAmount = Input.GetAxis("Horizontal") * turnSpeed;
+        float acceleration = Input.GetAxis("Vertical") * speed;
+
+        transform.Rotate(0, 0, -turnAmount);
+        transform.Translate(0, acceleration, 0);
     }
 }
